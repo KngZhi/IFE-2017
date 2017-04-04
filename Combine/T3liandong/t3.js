fe789039aed7aeb8cc2a92bbc5da8d4931dc9903
KngZhi <kngzhi@foxmail.com>
2017-04-04 15:23:12 +0800

    二次联动

--- Combine/T3liandong/t3.js
+++ Combine/T3liandong/t3.js
@@ -1,5 +1,7 @@
 var radios = document.querySelectorAll('input[name=fenlei]'),
-    len = radios.length
+    target = document.getElementById('college'),
+    source = document.getElementById('local');
+    len = radios.length;
 
 function toggle() {
     work = document.getElementById('work'),
@@ -7,16 +9,32 @@ function toggle() {
     if (this.value == 1) {
         sch.className = '';
         work.className = 'hidden';
-    } else if (this.value == 2) {
+    } else {
         sch.className = 'hidden';
         work.className = '';
-    }
+    };
 }
 
 function add() {
     for (var i=0;i<len; i++) {
         radios[i].addEventListener('click', toggle)
+    };
+}
+
+function select() {
+    var data = {
+        bj: ["北京大学", "清华大学"],
+        sh: ["南开大学", "上海大学"]
+    }
+        selected = source.options[source.selectedIndex].value;
+    target.innerHTML = "";
+    for (var i=0; i<data[selected].length; i++) {
+        var opt = document.createElement('option');
+        opt.value = data[selected][i];
+        opt.innerHTML = data[selected][i];
+        target.appendChild(opt);
     }
 }
+source.onchange = select;
 
 window.onload = add();
