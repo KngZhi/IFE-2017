1e2aca87f6204b7cebb5c477d89a21eeb318f759
KngZhi <kngzhi@foxmail.com>
2017-04-05 09:09:23 +0800

    修正规范

--- Combine/T2dynmaticValidation/t2.js
+++ Combine/T2dynmaticValidation/t2.js
@@ -1,6 +1,4 @@
 // BUG: 检验的方式比较 dirty
-
-
 // 为所有 input 添加事件
 function addListeners(event, func) {
     lists = document.getElementsByTagName('input');
@@ -13,12 +11,13 @@ addListeners('focus', toggleInfo);
 addListeners('blur', testInput);
 var btn = document.getElementsByTagName('button')[0];
 // 为什么点击 btn 时光标会自动跳转到最后一项 Input 中, 然后导致无法触发 blur 事件
-btn.onclick = function() {
+btn.onclick = function () {
     testAll();
     return false;
-}
+};
 
-var info, char;
+var info;
+var char;
 function notice(msg, style) {
     info.textContent = msg;
     info.className = style;
@@ -26,66 +25,57 @@ function notice(msg, style) {
 
 function testAll() {
     var content = document.getElementsByTagName('input');
-    for (var i=0, len=content.length; i<len ; i++) {
+    for (var i = 0, len = content.length; i < len; i++) {
         // Thing: 无需点击直接触发事件 x.event();
+        // BUG: 为什么 blur() 没有反应?
         content[i].focus();
     }
     // 防止光标停留在 content[len] 上
     btn.focus();
-    var notes = document.getElementsByClassName('right')
+    var notes = document.getElementsByClassName('right');
     if (notes.length > 4) {
         alert('格式输入正确');
-    } else {
-        alert('格式输入错误');
-    }
+    } else alert('格式输入错误');
 }
 
 function testChar(char) {
-    var len = getStrLen(char),
-        sg = '请参照正确格式书写',
-        right = '姓名格式正确',
-        wrong = '姓名不能为空'
+    var len = getStrLen(char);
+    var sg = '请参照正确格式书写';
+    var right = '姓名格式正确';
+    var wrong = '姓名不能为空';
     if (len === 0) {
         notice(wrong, 'wrong');
     } else if (len > 3 && len < 17) {
         notice(right, 'right');
-    } else {
-        notice(sg, '');
-    }
+    } else notice(sg, '');
 }
 
 function testRePsd() {
     if (org_psd === '') {
-        notice('原密码尚未输入', 'wrong')
-    }else if (char === org_psd) {
-        notice('两次密码一致', 'right')
-    } else {
-        notice('密码不一致', 'wrong')
-    }
+        notice('原密码尚未输入', 'wrong');
+    } else if (char === org_psd) {
+        notice('两次密码一致', 'right');
+    } else notice('密码不一致', 'wrong');
 }
 
 function testEmail() {
     var emailID = document.getElementById('email').value;
-    atpos = emailID.indexOf("@");
-    dotpos = emailID.lastIndexOf(".");
-    if (atpos < 1 || ( dotpos - atpos < 2 )) {
+    var atpos = emailID.indexOf('@');
+    var dotpos = emailID.lastIndexOf('.');
+    if (atpos < 1 || (dotpos - atpos < 2)) {
         notice('请输入正确的格式', 'wrong');
-    } else {
-        notice('输入正确!', 'right');
-    }
+    } else notice('输入正确!', 'right');
 }
 
-
-
 function testInput() {
-    char = this.value
+    char = this.value;
     // 手机号码为13位.
-    tel_reg = /^1(3|4|5|7|8)[0-9]\d{8}$/,
+    var tel_reg = /^1(3|4|5|7|8)[0-9]\d{8}$/;
     // 有一个不等的大写\小写\数字,总长度为6-13
-    psd_reg = /^\d{6,14}$/,
-    org_psd = document.getElementById('psd').value,
+    var psd_reg = /^\d{6,14}/;
+    org_psd = document.getElementById('psd').value;
     info = this.parentNode.lastElementChild;
-    type = this.name;
+    var type = this.name;
     this.nextElementSibling.className = 'hidden';
     switch (type) {
         case 'name':
@@ -94,12 +84,10 @@ function testInput() {
         case 'psd':
             if (psd_reg.test(char)) {
                 notice('密码格式正确', 'right');
-            } else {
-                notice('密码格式错误', 'wrong');
-            }
+            } else notice('密码格式错误', 'wrong');
             break;
         case 'repsd':
-            testRePsd()
+            testRePsd();
             break;
         case 'email':
             testEmail();
@@ -107,10 +95,8 @@ function testInput() {
         case 'tel':
             if (tel_reg.test(char)) {
                 notice('电话号码格式正确', 'right');
-            } else {
-                notice('电话号码格式错误', 'wrong');
-            }
-            break;
+            } else notice('电话号码格式错误', 'wrong');
+        break;
         default: return false;
     }
 }
@@ -124,11 +110,9 @@ function getStrLen(str) {
         // 如果字符的位置小于256(0~255 即为 ASCII 码);
         if (char < 0xFF) {
             len++;
-        } else {
-            len += 2
-        }
+        } else len += 2
     }
-    return len
+    return len;
 }
 
 /**
@@ -137,9 +121,9 @@ function getStrLen(str) {
  */
 
 function toggleInfo() {
-    hook = this.nextElementSibling;
-    biaoji = hook.className;
-    next_hook = hook.nextElementSibling;
+    var hook = this.nextElementSibling;
+    var biaoji = hook.className;
+    var next_hook = hook.nextElementSibling;
     // 同时检测两项指标
     switch (biaoji) {
         case 'hidden':
@@ -147,6 +131,6 @@ function toggleInfo() {
         case 'showup':
             next_hook.className = 'hidden'
             break;
-        default: ;
+        default:
     }
 }
