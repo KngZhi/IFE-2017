d03a234a061781c81c4a62c7e622ba74624fcc99
KngZhi <kngzhi@foxmail.com>
2017-04-09 15:56:20 +0800

    最新的版本

--- Combine/T2dynmaticValidation/t2.js
+++ Combine/T2dynmaticValidation/t2.js
@@ -1,7 +1,10 @@
-// BUG: 检验的方式比较 dirty
-// 为所有 input 添加事件
+'use strict';
+// var form = document.getElementById('form');
+// form.addEventListener('click', toggleInfo, false);
+// form.addEventListener('blur', testInput, false);
+
 function addListeners(event, func) {
-    lists = document.getElementsByTagName('input');
+    var lists = document.getElementsByTagName('input');
     for (var i=0, len=lists.length; i<len;i++) {
         lists[i].addEventListener(event, func, false);
     }
@@ -10,17 +13,16 @@ function addListeners(event, func) {
 addListeners('focus', toggleInfo);
 addListeners('blur', testInput);
 var btn = document.getElementsByTagName('button')[0];
-// 为什么点击 btn 时光标会自动跳转到最后一项 Input 中, 然后导致无法触发 blur 事件
 btn.onclick = function () {
     testAll();
     return false;
 };
 
-var info;
-var char;
+
 function notice(msg, style) {
-    info.textContent = msg;
-    info.className = style;
+    var position = this.parentNode.lastElementChild;
+    position.textContent = msg;
+    position.className = style;
 }
 
 function testAll() {
@@ -38,8 +40,40 @@ function testAll() {
     } else alert('格式输入错误');
 }
 
-function testChar(char) {
-    var len = getStrLen(char);
+function notice(msg, style) {
+    var position = this.parentNode.lastElementChild;
+    position.textContent = msg;
+    position.className = style;
+}
+
+
+// 验证输入的信息
+function testInput() {
+    var char = this.value.trim();
+    position = this.parentNode.lastElementChild;
+    this.nextElementSibling.className = 'hidden';
+    switch (this.name) {
+        case 'name':
+            testChar(char);
+            break;
+        case 'psd':
+            testPsd(char)
+            break;
+        case 'repsd':
+            testRePsd(char);
+            break;
+        case 'email':
+            testEmail(char);
+            break;
+        case 'tel':
+            testTel(char);
+        break;
+        default: return false;
+    }
+}
+
+// 验证姓名
+function testChar(len) {
     var sg = '请参照正确格式书写';
     var right = '姓名格式正确';
     var wrong = '姓名不能为空';
@@ -47,58 +81,7 @@ function testChar(char) {
         notice(wrong, 'wrong');
     } else if (len > 3 && len < 17) {
         notice(right, 'right');
-    } else notice(sg, '');
-}
-
-function testRePsd() {
-    if (org_psd === '') {
-        notice('原密码尚未输入', 'wrong');
-    } else if (char === org_psd) {
-        notice('两次密码一致', 'right');
-    } else notice('密码不一致', 'wrong');
-}
-
-function testEmail() {
-    var emailID = document.getElementById('email').value;
-    var atpos = emailID.indexOf('@');
-    var dotpos = emailID.lastIndexOf('.');
-    if (atpos < 1 || (dotpos - atpos < 2)) {
-        notice('请输入正确的格式', 'wrong');
-    } else notice('输入正确!', 'right');
-}
-
-function testInput() {
-    char = this.value;
-    // 手机号码为13位.
-    var tel_reg = /^1(3|4|5|7|8)[0-9]\d{8}$/;
-    // 有一个不等的大写\小写\数字,总长度为6-13
-    var psd_reg = /^\d{6,14}/;
-    org_psd = document.getElementById('psd').value;
-    info = this.parentNode.lastElementChild;
-    var type = this.name;
-    this.nextElementSibling.className = 'hidden';
-    switch (type) {
-        case 'name':
-            testChar(char);
-            break;
-        case 'psd':
-            if (psd_reg.test(char)) {
-                notice('密码格式正确', 'right');
-            } else notice('密码格式错误', 'wrong');
-            break;
-        case 'repsd':
-            testRePsd();
-            break;
-        case 'email':
-            testEmail();
-            break;
-        case 'tel':
-            if (tel_reg.test(char)) {
-                notice('电话号码格式正确', 'right');
-            } else notice('电话号码格式错误', 'wrong');
-        break;
-        default: return false;
-    }
+    } else notice(sg, 'wrong');
 }
 
 function getStrLen(str) {
@@ -115,21 +98,51 @@ function getStrLen(str) {
     return len;
 }
 
-/**
- * 给 Span 添加 .showup;
- * @return {[type]} [description]
- */
+// 验证密码
+function testPsd(value) {
+    var psd_reg = /^\d{6,14}/;
+    if (psd_reg.test(value)) {
+        notice('密码格式正确', 'right');
+    } else notice('请按格式要求输入内容', 'wrong');
+}
 
+// 第二次验证密码
+function testRePsd(value) {
+    var org_psd = document.getElementById('psd').value;
+    if (!org_psd) {
+        notice('原密码尚未输入', 'wrong');
+    } else if (value === org_psd) {
+        notice('两次密码一致', 'right');
+    } else notice('密码不一致', 'wrong');
+}
+
+// 验证邮箱
+function testEmail(value) {
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (filter.test(value)) {
+        notice('输入正确!', 'right');
+    } else notice('请按格式要求输入内容', 'wrong');
+}
+
+// 验证手机
+function testTel(value) {
+    var filter = /^(13[0-9]|14[0-9]|15[0-9]|18[0-9])\d{8}$/;
+    if (filter.test(value)) {
+        notice('输入正确', 'right');
+    } else notice('请按格式要求输入内容', 'wrong')
+}
+
+// 提示信息
 function toggleInfo() {
     var hook = this.nextElementSibling;
-    var biaoji = hook.className;
-    var next_hook = hook.nextElementSibling;
+    var class_name = hook.className;
+    var next_class_name = hook.nextElementSibling;
     // 同时检测两项指标
-    switch (biaoji) {
+    switch (class_name) {
         case 'hidden':
             hook.className = 'showup';
         case 'showup':
-            next_hook.className = 'hidden'
+            next_class_name.className = 'hidden'
             break;
         default:
     }
